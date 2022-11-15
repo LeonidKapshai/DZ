9557358a2e59df78bcf48b85f0fb0aede6360f2b
Kapshai <leonid.kapshai@gmail.com>
2022-11-16 02:25:13 +0300

    решенные задачи

--- 3dz/Program.cs
+++ 3dz/Program.cs
@@ -2,9 +2,9 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 if (number1 % 2 == 0)
 {
-    Console.Writeline("Число четное");
+    Console.WriteLine("Число четное");
 }
 else 
 {
-    Console.Writeline("Число не четное");
+    Console.WriteLine("Число не четное");
 }

1e11b731a88c1f1c212c6e48903d73298d9557b6
Kapshai <leonid.kapshai@gmail.com>
2022-11-16 01:42:05 +0300

    1,2,3 Задачи

---
+++ 3dz/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.Writeline("Число четное");
+}
+else 
+{
+    Console.Writeline("Число не четное");
+}
